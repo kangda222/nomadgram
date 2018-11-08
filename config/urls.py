834bbe9dbd13d51d0b704f7305addb6201ed8a1e
DaeunKang <jeangrey@xhiftcorp.com>
2018-11-08 15:16:45 +0900

    setting up JWT

--- config/urls.py
+++ config/urls.py
@@ -4,17 +4,16 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django.views import defaults as default_views
+from rest_framework_jwt.views import obtain_jwt_token
 
-urlpatterns = [
-    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
-    path(
-        "about/",
-        TemplateView.as_view(template_name="pages/about.html"),
-        name="about",
-    ),
+urlpatterns = [    
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
-    # User management
+    # User management    
+    path(
+        "api-token-auth/",
+        obtain_jwt_token
+    ),
     path(
         "users/",
         include("nomadgram.users.urls", namespace="users"),
