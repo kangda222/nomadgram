0baedbd257313772b2cb2b6c9878a72ccbcc9224
DaeunKang <jeangrey@xhiftcorp.com>
2018-11-15 11:24:06 +0900

    Moving the App Component and Adding Reset-CSS

--- fronted/src/index.js
+++ fronted/src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/App';
 import { Provider } from "react-redux";
 import { ConnectedRouter } from "react-router-redux";
 import store, { history } from "./redux/configureStore";
 import I18n from "redux-i18n";
 import { translations } from "./translations";
-//import "./ReactotronConfig";
 
 ReactDOM.render(
   <Provider store={store}>
