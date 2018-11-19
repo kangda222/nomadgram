5943562d84fb4dc3276a03ed5eb132007bb7080f
DaeunKang <jeangrey@xhiftcorp.com>
2018-11-19 11:23:51 +0900

    Stateless Components and Context

--- fronted/src/components/AuthForms/index.js
+++ fronted/src/components/AuthForms/index.js
@@ -1,28 +1,32 @@
 import React from "react";
 import Ionicon from "react-ionicons";
+import Proptypes from "prop-types";
 import styles from "./styles.module.scss";
 
-export const LoginForm = props => (
+export const LoginForm = (props, context) => (
     <div className={styles.formComponent}>        
         <form className={styles.form}>
-        <input type="text" placeholder="Username" className={styles.textInput} />
+        <input type="text" placeholder={context.t("Username")} className={styles.textInput} />
         <input
             type="password"
-            placeholder="Password"
+            placeholder={context.t("Password")}
             className={styles.textInput}
         />
-        <input type="submit" value="Log in" className={styles.button} />
+        <input type="submit" value={context.t("Log in")} className={styles.button} />
         </form>
-        <span className={styles.divider}>or</span>
+        <span className={styles.divider}>{context.t("or")}</span>
         <span className={styles.facebookLink}>
-        <Ionicon icon="logo-facebook" fontSize="20px" color="#385185" /> Log in
-        with Facebook
+        <Ionicon icon="logo-facebook" fontSize="20px" color="#385185" /> {context.t("Log in with Facebook")}
         </span>
-        <span className={styles.forgotLink}>Forgot password?</span>
+        <span className={styles.forgotLink}>{context.t("Forgot password?")}</span>
     </div>
 );
 
-export const SignupForm = props => (
+LoginForm.contextTypes = {
+    t: Proptypes.func.isRequired
+};
+
+export const SignupForm = (props, context) => (
     <div className={styles.formComponent}>
         <h3 className={styles.signupHeader}>
         Sign up to see photos and videos from your friends.
@@ -33,22 +37,26 @@ export const SignupForm = props => (
         </button>
         <span className={styles.divider}>or</span>
         <form className={styles.form}>
-        <input type="email" placeholder="Email" className={styles.textInput} />
-        <input type="text" placeholder="Full Name" className={styles.textInput} />
+        <input type="email" placeholder={context.t("Email")} className={styles.textInput} />
+        <input type="text" placeholder={context.t("Full Name")} className={styles.textInput} />
         <input
             type="username"
-            placeholder="Username"
+            placeholder={context.t("Username")}
             className={styles.textInput}
         />
         <input
             type="password"
-            placeholder="Password"
+            placeholder={context.t("Password")}
             className={styles.textInput}
         />
-        <input type="submit" value="Sign up" className={styles.button} />
+        <input type="submit" value={context.t("Sign up")} className={styles.button} />
         </form>
         <p className={styles.terms}>
         By signing up, you agree to our <span>Terms & Privacy Policy</span>.
         </p>
     </div>
 );
+
+SignupForm.contextTypes = {
+    t: Proptypes.func.isRequired
+};
--- fronted/src/translations.js
+++ fronted/src/translations.js
@@ -1,5 +1,20 @@
 export const translations = {
     es: {
         "Log in": "Iniciar Sesión"
+    },
+    kr: {
+        "Log in": "로그인",
+        "About Us": "소개",
+        "Support": "서포트",
+        "Blog": "블로그",
+        "Press": "프레스",
+        "API": "API",
+        "Jobs": "잡",
+        "Privacy": "프라이버시",
+        "Terms": "텀스",
+        "Directory": "디렉토리",
+        "Language": "언어",
+        "Username": "사용자명",
+        "Password": "패스워드"
     }
 };
