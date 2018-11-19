05a854c4492265eab33dc3d1d7d09f7520381036
DaeunKang <jeangrey@xhiftcorp.com>
2018-11-19 11:50:24 +0900

    Extracting and importing Translations

--- fronted/src/translations.js
+++ fronted/src/translations.js
@@ -1,20 +1,25 @@
 export const translations = {
-    es: {
-        "Log in": "Iniciar Sesión"
-    },
-    kr: {
-        "Log in": "로그인",
-        "About Us": "소개",
-        "Support": "서포트",
-        "Blog": "블로그",
-        "Press": "프레스",
-        "API": "API",
-        "Jobs": "잡",
-        "Privacy": "프라이버시",
-        "Terms": "텀스",
-        "Directory": "디렉토리",
-        "Language": "언어",
-        "Username": "사용자명",
-        "Password": "패스워드"
-    }
-};
+  'kr': {
+    'Username': '사용자명',
+    'Password': '패스워드',
+    'Log in': '로그인',
+    'or': '',
+    'Log in with Facebook': '',
+    'Forgot password?': '',
+    'Email': '이메일',
+    'Full Name': '성명',
+    'Sign up': '가입하기',
+    'About Us': '소개',
+    'Support': '서포트',
+    'Blog': '블로그',
+    'Press': '프레스',
+    'API': 'API',
+    'Jobs': '',
+    'Privacy': '프라이버시',
+    'Terms': '용어',
+    'Directory': '디렉토리',
+    'Language': '언어',
+  },
+  'options': {
+  },
+}
