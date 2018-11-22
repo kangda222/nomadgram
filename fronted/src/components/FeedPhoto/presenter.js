c7191134500978c4efc380afed8aa36a68ded2bd
DaeunKang <jeangrey@xhiftcorp.com>
2018-11-22 18:17:22 +0900

    FeedPhoto Component pt. Two

--- fronted/src/components/FeedPhoto/presenter.js
+++ fronted/src/components/FeedPhoto/presenter.js
@@ -1,10 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
+import PhotoActions from "components/PhotoActions";
 import styles from "./styles.module.scss";
 
 const FeedPhoto = (props, context) => {
     //console.log(props);
-    return <div className={styles.feedPhoto}>"hello"</div>;
+    return (
+        <div className={styles.feedPhoto}>
+          <header>
+            <img
+              src={props.creator.profile_image || require("images/noPhoto.png")}
+              alt={props.creator.username}
+            />
+            <div>
+              <span>{props.creator.username}</span>
+              <span>{props.location}</span>
+            </div>
+          </header>
+          <img src={props.file} alt={props.caption} />
+          <div>
+            <PhotoActions number={props.like_counts} />
+          </div>
+        </div>
+      );
 };
 
 FeedPhoto.propTypes = {
